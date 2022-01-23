b662bad243f6af39cf765db2fdccb3bac5533a0c
Himalaya <yugmani@yahoo.com>
2022-01-22 23:46:51 -0800

    started js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+getRandomMeal();
+
+async function getRandomMeal() {
+  const resp = await fetch("http://www.themealdb.com/api/json/v1/1/random.php");
+  const respData = await resp.json();
+  console.log(respData);
+}
+
+function getMealById(id) {
+  const meal = await fetch(
+    "www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+}
+
+function getMealsBySearch(term) {
+  const meals = await fetch(
+    "www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+}
